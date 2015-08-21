83d20dadfffaf852bfbc6eec1fa91bce3383f318
Priscilla Armand <prisc_000@MacBook-Pro.local>
2015-08-21 13:16:37 -0400

    Trying to get player sprite to show
    At this point, canvas is showing and bugs are moving but player is not
    showing.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function(loc) {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    var obj = {loc: loc};
-    obj.loc = loc;
-    return obj;
-
+    this.x = x;
+    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-
     this.sprite = 'images/enemy-bug.png';
+   
 }
 
 // Update the enemy's position, required method for game
@@ -18,6 +16,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + 10 * dt;
+    if (this.x > 505) {
+        this.x = -10;
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -28,30 +30,45 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-function Player(x,y,dt) {
+var Player = function(x,y){
     this.x = x;
     this.y = y;
-    this.sprite = 'images/char-cat-girl.png';
+    this.sprite = 'images/char-boy.png';
 }
-Player.prototype = Object.create(Enemy.prototype);
-Player.prototype.constructor = Player;
 
 
+Player.prototype.update = function (dt) {
+    this.x = this.x + 10 * dt;
+}
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(allowedKeys){
+    
+    switch(allowedKeys){
+        case 'left' : if(this.x>0){this.x=this.x-10;}
+                        break;
+        case 'right': if(this.x<404){this.x=this.x+10;}
+                        break;
+        case 'up': this.y=this.y-10;
+                        break;
+        case 'down': if(this.y<375){this.y=this.y+10;}
+                        break;
+            }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-var enemy = new Enemy(202, 0);
-allEnemies[allEnemies.length] = enemy;
-var enemy1 = new Enemy(303, 0);
-allEnemies[allEnemies.length] = enemy1;
-var enemy2 = new Enemy(404, 0);
-allEnemies[allEnemies.length] = enemy2;
-var enemy3 = new Enemy(303, 252);
-allEnemies[allEnemies.length] = enemy3;
-var player = new Player(0, 252);
+allEnemies[0] = new Enemy (0, 65);
+allEnemies[1] = new Enemy (0, 150);
+allEnemies[2] = new Enemy (0, 245);
+var player = new Player(202, 305);
+
+
 
 
 // This listens for key presses and sends the keys to your
